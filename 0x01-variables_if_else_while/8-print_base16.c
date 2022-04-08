6a73d734e0b6f914e2c24e8e04e0fa7b029e6237
yonathangit <yonathan.berhanu17@gmail.com>
2022-04-08 12:49:36 +0300

    My tenth task commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,24 @@
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Return:0
  */
 int main(void)
 {
-	int d;
-	char low;
 
-	for (d = '0'; d <= '9'; d++)
-		putchar(d);
-	for (low = 'a'; low <= 'f'; low++)
-		putchar(low);
-		putchar('\n');
+	int i, y;
 
-	return (0);
+	for (i = 0; i < 10; i++)
+	{
+		putchar (i + '0');
+	}
+	for (y = 'a'; y <= 'f'; y++)
+	{
+		putchar (y);
+	}
+
+putchar('\n');
+return (0);
 }
